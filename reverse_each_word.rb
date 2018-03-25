db59c0a1b16b975d7dbc40887a06fcf7e91c8c09
Megan Dotto <megan.dotto@gmail.com>
2018-03-25 03:37:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
-  sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word, word.reverse|}
-  new_array.join(" ")
+  new_array = sentence.split(" ")
+  new_array.collect do |word|
+    word.reverse
+  end
+  return .join(" ")
 end
-  
